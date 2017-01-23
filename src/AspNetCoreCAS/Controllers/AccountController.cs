321dbebf0acf8a0d27d85603636378513db017e5
Diego Wang <diegowang@yahoo.com>
2017-01-23 16:24:58 -0500

    The AccountController declares three private variables of type UserManager<T>, SignInManager<T> and RoleManager<T> respectively. The UserManager class is used for creating and managing users. The RoleManager class is used for creating and managing roles. The SignInManager class is used to log a user in and out of the system. Object instances of these three classes are injected into the AccountController constructor. The constructor code simply assigns the injected objects to the respective variables declared earlier.

---
+++ src/AspNetCoreCAS/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿
+using AspNetCoreCAS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreCAS.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        //private readonly IEmailSender _emailSender;
+        //private readonly ISmsSender _smsSender;
+        //private readonly ILogger _logger;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        
+    }
+}
